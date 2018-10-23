23e2e8529e1f4c09c5db8442b3a743b31c54b1b9
Aaron277 <1210564879@qq.com>
2018-10-23 20:43:29 +0800

    Update app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,4 +2,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:123456@127.0.0.1:3306/flask?charset=utf8'
 
+db = SQLAlchemy(app)
